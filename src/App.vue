e9995a7ab0aced998038b440fa54541bbf6a838b
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-07-19 15:40:24 +0800

    input_for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
+
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+
+        <input v-model.number="count" placeholder="2" > <br> <br>
+
+<!--        
+        <button @click="increment">+</button>         
+        <span >{{count}}</span> 
+        <button @click="decrement">-</button>
+       
+  <ul id="example-1">
+    <li v-for="item in count "  v-bind:key="item">
+        {{ item }}  
+    </li>
+  </ul> -->
+
+      <div v-for="item in count "  v-bind:key="item"> 
+              <button @click="increment">+</button>         
+              <span >{{count}}</span>  
+              <button @click="decrement">-</button>
+      </div>
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name:'app',
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+        data(){
+            return {
+                count: 0
+            }
+        },
+        methods: {  
+            increment(){
+                this.count++;
+            },
+            decrement(){
+                this.count--;  
+            }
+        }
+    }
+
+
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
